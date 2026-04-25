6913230ded83a73009c004c5c854227bd7b7d8c7
mr-smoothy <asifulislam2k04@gmail.com>
2026-04-25 06:20:52 +0600

    Update API base URL to use environment variable

--- frontendip/src/services/api.js
+++ frontendip/src/services/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 let lastNetworkErrorToastAt = 0;
 
 const api = axios.create({
-  baseURL: 'http://localhost:8080/api',
+  baseURL: `${process.env.REACT_APP_API_URL}/api',
   headers: {
     'Content-Type': 'application/json',
   },
