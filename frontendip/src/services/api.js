d4553150f1412c14beceea39506b5a18237c8e04
mr-smoothy <asifulislam2k04@gmail.com>
2026-04-25 07:00:35 +0600

    Fix template string syntax for baseURL in api.js

--- frontendip/src/services/api.js
+++ frontendip/src/services/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 let lastNetworkErrorToastAt = 0;
 
 const api = axios.create({
-  baseURL: '${process.env.REACT_APP_API_URL}/api',
+  baseURL: `${process.env.REACT_APP_API_URL}/api`,
   headers: {
     'Content-Type': 'application/json',
   },
